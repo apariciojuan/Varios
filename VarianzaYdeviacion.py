86dc4df38819bbe56ba3c3d83564c73d68e5d6c5
juan <yoshua66angel@yahoo.com.ar>
2017-12-05 15:46:42 +0100

    Ejercicio Varianza y Desviacion de estadistica en python 3

---
+++ VarianzaYdeviacion.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import math
+datos = [13, 14, 15, 15, 15, 16, 17, 18, 20]
+
+def varianza(datos):
+    sumatoria = sum(datos)
+    media = sumatoria / len(datos)
+    rr = list(map(lambda x: (x - media)**2, datos))
+    sumatoria2 = sum(rr) / len(rr)
+    return sumatoria2
+
+
+def desviacion(datos):
+    datos = varianza(datos)
+    return math.sqrt(datos)
+
+print(varianza(datos))
+print(desviacion(datos))
+
+
+
+
+
+
+
+
